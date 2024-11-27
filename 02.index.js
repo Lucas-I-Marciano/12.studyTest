00089002020f13a431e65ac4898c99fb85b08e67
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2024-11-26 21:43:34 -0300

    :books: docs: adding Jest

--- 02.index.js
+++ 02.index.js
@@ -1,40 +1,5 @@
-const contabilizaSalario = (salario, horaExtra) => {
+export const contabilizaSalario = (salario, horaExtra) => {
   return salario + horaExtra;
 };
 
-const calculaDesconto = (salario, descontos) => salario - descontos;
-
-function verifiqueSe(valor) {
-  const assercoes = {
-    ehExatamente: (esperado) => {
-      if (valor != esperado) {
-        throw new Error("Valores Diferentes");
-      }
-    },
-    ehMenor: (esperado) => {
-      if (valor >= esperado) {
-        throw new Error("Valor maior que o esperado");
-      }
-    },
-  };
-  return assercoes;
-}
-
-function teste(titulo, funcao) {
-  try {
-    funcao();
-    console.log(`${titulo} Passou!`);
-  } catch (erro) {
-    console.error(`${titulo} não passou!!! ${erro.message}`);
-  }
-}
-
-teste("calculaDesconto", () => {
-  verifiqueSe(2000).ehExatamente(calculaDesconto(2500, 500));
-});
-teste("contabilizaSalario", () => {
-  verifiqueSe(2000).ehExatamente(contabilizaSalario(123123, 500));
-});
-teste("contabilizaSalario", () => {
-  verifiqueSe(2000).ehMenor(contabilizaSalario(123123, 500));
-});
+export const calculaDesconto = (salario, descontos) => salario - descontos;
