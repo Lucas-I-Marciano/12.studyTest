8a861d0afb2f1f947629208bf292f2e1c50b86c8
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2024-12-01 19:48:13 -0300

    :books: test-supertest: 'Should return list of Editoras'

--- api/src/test/routes/editoraRoutes.test.js
+++ api/src/test/routes/editoraRoutes.test.js
@@ -1,19 +1,26 @@
 import app from "../../app.js";
+import request from "supertest";
+import { expect } from "@jest/globals";
 
 const port = 3000;
 
 let server;
 
 beforeEach(() => {
-  server = app.listen(port, console.log("Server executing...3000"));
+  server = app.listen(port);
 });
 
 afterEach(() => {
   server.close();
 });
 
-describe("Testes em /editoras", () => {
-  it("Deve retornar lista de editoras", () => {
-    expect(2).toBe(2);
+describe("GET em /editoras", () => {
+  it("Deve retornar lista de editoras", async () => {
+    const dado = await request(server)
+      .get("/editoras")
+      .expect("Content-Type", /json/)
+      .expect(200); // expect do supertest
+
+    expect(dado.body[0]["email"]).toStrictEqual(expect.any(String)); // expect normal
   });
 });
