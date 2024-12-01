e44ec9e7e56f1ec4fcbc737156005a8d25115842
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2024-12-01 16:59:56 -0300

    :books: test: 'Should simulate create function

--- api/src/test/models/editora.test.js
+++ api/src/test/models/editora.test.js
@@ -1,4 +1,5 @@
 import Editora from "../../models/editora.js";
+import { expect, jest, test, it } from "@jest/globals";
 
 describe("Testes do Modelo Editora", () => {
   const objetoEditora = {
@@ -11,11 +12,31 @@ describe("Testes do Modelo Editora", () => {
     expect(editora).toEqual(expect.objectContaining(objetoEditora));
   });
 
-  it("Deve salvar no BD", async () => {
+  it.skip("Deve salvar no BD", async () => {
     const editora = new Editora(objetoEditora);
     const dados = await editora.salvar(editora);
 
     expect(dados).toEqual(
+      expect.objectContaining({
+        id: expect.any(Number),
+        ...objetoEditora,
+        created_at: expect.any(String),
+        updated_at: expect.any(String),
+      })
+    );
+  });
+
+  it("Deve salvar no BD - Usando Mock/Spies", () => {
+    const mockFn = jest.fn().mockReturnValue({
+      id: 10,
+      nome: "Lucas' publisher",
+      cidade: "SJC",
+      email: "LP@gmail.com",
+      updated_at: "2024-10-10",
+      created_at: "2024-10-10",
+    });
+
+    expect(mockFn()).toEqual(
       expect.objectContaining({
         id: expect.any(Number),
         ...objetoEditora,
